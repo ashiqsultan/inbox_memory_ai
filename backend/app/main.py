78993d6c6706e496093f1f78367b8848fa84f7a2
Ashiq Sultan <ashiqdrive@gmail.com>
2025-05-28 16:22:45 +0000

    Added origins again

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["https://kbhelper.com"],
+    allow_origins=["https://kbhelper.com", "https://www.kbhelper.com"],
 )
 
 
