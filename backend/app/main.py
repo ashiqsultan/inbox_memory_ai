36a15a6d0ea179128c308fac800c5185b67886ef
Ashiq Sultan <ashiqdrive@gmail.com>
2025-05-28 16:20:55 +0000

    Added origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,11 @@ app = FastAPI(
 
 # Add CORS middleware
 app.add_middleware(
-    CORSMiddleware, allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_origins=["https://kbhelper.com"],
 )
 
 
